f9298e05dd03fadef846d63daea08952df883918
alpeza <80060968+alpeza@users.noreply.github.com>
2023-04-10 17:36:07 +0200

    up

--- hcilinks_back/app.py
+++ hcilinks_back/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, jsonify
-from flask_restful import Resource, Api
+from flask import Flask
+from flask_restful import Api
 from dotenv import load_dotenv
 from endpoints import links
 
